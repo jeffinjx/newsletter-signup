8a31f83751b19b4baa467ad8fba1e601c10c026a
Jeffin Jaison <jeffinjaison.9@gmail.com>
2022-07-01 06:16:50 +0300

    added new manual PORT in app.listen

--- app.js
+++ app.js
@@ -57,6 +57,6 @@ app.post("/", function(req, res){
 });
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("server is now running in port 3000");
 });
